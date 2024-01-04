1810b92b4bf25a7bf70d4d4fc0707acc13814708
Shubham Bhardwaj <94232997+bhardwaj-shubham@users.noreply.github.com>
2024-01-04 20:08:08 +0530

    Creates config struct and separate config's code from main function

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,32 @@ use std::{env, fs::File, io::Read};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("Search query: {}", query);
-    println!("Search in {} file", filename);
+    println!("Search query: {}", config.query);
+    println!("Search in {} file", config.filename);
 
-    let mut f = File::open(filename).expect("file not found!");
+    let mut f = File::open(config.filename).expect("file not found!");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments!");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
